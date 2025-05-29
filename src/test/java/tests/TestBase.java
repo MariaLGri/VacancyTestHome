a7cd5951c4631c622b1dd87cdfd434a578ddc85a
MLGrishina <maria107aa@gmail.com>
2025-05-29 14:19:34 +0300

    changes file

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,23 +4,55 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
     @BeforeAll
     static void installСonfiguration() {
-        Configuration.browserSize = "1920x1080"; // разрешение, ну это понятно
-        Configuration.pageLoadStrategy = "eager"; // чтоб не ждать загрузки всего сайта , картинок и т.д
-        Configuration.baseUrl = "https://penza.hh.ru"; // выносим абсолютный адресс из опен
-       Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //хром на стороннем сервисе
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String screenResolution = System.getProperty("screenResolution", "1920x1080");
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = screenResolution;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = String.format(
+                "https://%s:%s@%s/wd/hub",
+                System.getProperty("selenoidUserLogin", "user1"),
+                System.getProperty("selenoidUserPassword", "1234"),
+                System.getProperty("selenoidUrl", "selenoid.autotests.cloud")
+        );
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
-    @AfterEach
-    void webDriverTearDown() {
-        Selenide.closeWebDriver();
-    }
 
+
+    @AfterEach
+
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        //Selenide.closeWebDriver();
+    }
 }
+
